312e5cdf508fd39e8682f109b20da6a711573804
Rafael Nicdao <nicdaoraf@gmail.com>
2021-03-07 11:32:35 +1100

    Define keymaps

---
+++ src/comfykafka/transient/keys.cljs
@@ -0,0 +1,30 @@
+(ns comfykafka.transient.keys)
+
+(def topic-keymap
+  ["t" :topics/view "Topics"
+   ["o" :topic/overview "Topic overview"]
+
+   ["w" :topic/write "Write a Message"
+    ["x" :message/write-external "Open external editor"]
+    ["y" :topic/write-confirm "Yes, write"]
+    ["n" :topic/write-cancel "Cancel"]]
+
+   ["r" :topic/read "Read Messages"
+    ["p" :partitions/select "Select a Partition"
+     ["m" :message/view "Inspect Message"
+      ["c" :message/copy "Copy"]
+      ["x" :message/read-external "Open external editor"]]]]])
+
+(def workflow-keymap
+  ["c" :connections/view "Connections"
+   ["c" :connection/connect "Connect"
+    topic-keymap]
+   ["n" :connection/create "New"
+    ["s" :connection/save "Save"]]
+   ["e" :connection/edit "Edit"
+    ["s" :connection/save "Save"]]
+   ["d" :connection/delete "Delete"
+    ["y" :connection/delete-confirm "Yes, delete"]
+    ["n" :connection/delete-cancel "Cancel"]]])
+
+(def go-back-key "g")
