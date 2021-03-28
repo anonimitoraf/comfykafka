f3000fc32884fc132f7daa1b6ba30f7245327612
Rafael Nicdao <nicdaoraf@gmail.com>
2021-03-28 23:20:58 +1100

    Some work on persistent DB

--- src/comfykafka/wrappers/persistent_db.cljs
+++ src/comfykafka/wrappers/persistent_db.cljs
@@ -1,46 +1,57 @@
 (ns comfykafka.wrappers.persistent-db
-  (:require [cljs.core.async :refer [go]]
-            [cljs.core.async.interop :refer-macros [<p!]]
+  (:require [cljs.core.async
+             :refer [chan >! <!]
+             :refer-macros [go go-loop]]
             ["path" :as path]
             ["os" :as os]
             ["nedb" :as Datastore]
             [comfykafka.errors :refer [->AsyncError]]))
 
-(def ^:private db-file-dir (.join path (.homedir os)
-                                  ".comfykafka/nedb/"))
-(def ^:private connections-db
-  (new Datastore (clj->js {:filename (str db-file-dir "connections.db")
-                           :autoload true})))
+(defrecord DBError [msg details])
+
+(def dbs (atom {}))
 
 (defn ^:private resolve-db
   [db-identifier]
-  (condp = db-identifier
-    :connections connections-db))
+  (or (db-identifier @dbs)
+      (let [db (new Datastore (clj->js {:filename
+                                        (str (.join path (.homedir os) ".comfykafka/nedb/")
+                                             (name db-identifier) ".db")
+                                        :autoload true}))]
+        (swap! dbs assoc db-identifier db)
+        db)))
 
 ;; TODO Spec to check that doc has :id
 (defn upsert
-  "Upserts `doc` into `db`. Returns a Promise containing the new doc"
+  "Upserts `doc` into `db`.
+  Returns a channel which emits either:
+  * The upserted doc is successful
+  * An `->AsyncError` if not"
   [db doc]
-  (go (<p! (new js/Promise
-                (fn [resolve reject]
-                  (.update (resolve-db db)
-                           (clj->js {"_id" (doc :id)})
-                           (clj->js (-> doc
-                                        (dissoc :id)
-                                        (merge {"_id" (:id doc)})))
-                           (clj->js {:upsert true})
-                           (fn [err new-doc] (if err
-                                               (reject (->AsyncError (.-message err)
-                                                                     (.-stack err)))
-                                               (resolve new-doc)))))))))
+  (let [ch (chan)]
+    (.update (resolve-db db)
+             (clj->js {"_id" (doc :id)})
+             (clj->js (-> doc
+                          (dissoc :id)
+                          (merge {"_id" (:id doc)})))
+             (clj->js {:upsert true
+                       :multi false
+                       :returnUpdatedDocs true})
+             (fn [err _affected-count affected-docs]
+               (go (>! ch (if (nil? err)
+                            (js->clj affected-docs)
+                            (->DBError (.-message err) (.-stack err)))))))
+    ch))
 
 (defn get-all
-  "Returns a Promise containing all the docs in `db`"
+  "Returns a channel which emits either:
+  * All the docs in the `db`
+  * An `->AsyncError` if not"
   [db]
-  (go (<p! (new js/Promise
-                (fn [resolve reject]
-                  (.find (resolve-db db) (clj->js {})
-                         (fn [err docs] (if err
-                                          (reject (->AsyncError (.-message err)
-                                                                (.-stack err)))
-                                          (resolve docs)))))))))
+  (let [ch (chan)]
+    (.find (resolve-db db) (clj->js {})
+           (fn [err docs]
+             (go (>! ch (if (nil? err)
+                          (map js->clj (js->clj docs))
+                          (->DBError (.-message err) (.-stack err)))))))
+    ch))
